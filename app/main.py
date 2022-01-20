52928fe48c4f9c5f6a6e6e6739b434c966d5e238
suyog2398 <suyogkapdekar.23@gmail.com>
2022-01-20 13:56:36 +0530

    main change

--- app/main.py
+++ app/main.py
@@ -24,10 +24,7 @@ app = FastAPI()
 #     "https://localhost.tiangolo.com",
 #     "http://localhost",
 #     "http://localhost:8080",
-origins = ["*", "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
